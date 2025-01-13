302fdb3136d4e640874848fe6cf7e41953bee4de
Ravi Shrestha <rshrestha42@gatech.edu>
2025-01-12 20:01:22 -0600

    Initial setup for virtual DOM creation and mounting DOM

---
+++ packages/runtime/src/mount-dom.js
@@ -0,0 +1,31 @@
+import { DOM_TYPES } from "./h";
+
+export function mountDOM(vdom, parentEl){
+    switch (vdom.type){
+        case DOM_TYPES.TEXT: {
+            createTextNode(vdom, parentEl)
+            break
+        }
+
+        case DOM_TYPES.ELEMENT: {
+            createElementNode(vdom, parentEl)
+            break
+        }
+
+        case DOM_TYPES.FRAGMENT: {
+            createFragmentNodes(vdom, parentEl)
+            break
+        }
+
+        default: {
+            throw new Error(`Can't mount DOM of type: ${vdom.type}`)
+        }
+    }
+}
+
+
+// TODO: implement createTextNode()
+
+// TODO: implement createElementNode()
+
+// TODO: implement createFragmentNodes()
