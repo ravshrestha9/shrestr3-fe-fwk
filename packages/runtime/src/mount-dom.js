4436a374a0d15a202188daa2f02deed18060dc95
Ravi Shrestha <rshrestha42@gatech.edu>
2025-01-13 20:40:10 -0600

    implement mounting elements

--- packages/runtime/src/mount-dom.js
+++ packages/runtime/src/mount-dom.js
@@ -1,4 +1,6 @@
 import { DOM_TYPES } from "./h";
+import { setAttributes } from '/attributes'
+import { addEventListeners } from './events'
 
 export function mountDOM(vdom, parentEl){
     switch (vdom.type){
@@ -24,8 +26,38 @@ export function mountDOM(vdom, parentEl){
 }
 
 
-// TODO: implement createTextNode()
+function createTextNode(vdom, parentEl){
+    const {value} = vdom
 
-// TODO: implement createElementNode()
+    const textNode = document.createTextNode(value)
+    vdom.el = textNode
 
-// TODO: implement createFragmentNodes()
+    parentEl.append(textNode)
+}
+
+
+function createFragmentNodes(vdom, parentEl){
+    const { children } = vdom
+    vdom.el = parentEl
+    
+    children.forEach((child)=> mountDOM(child, parentEl))
+}
+
+
+function createElementNode(vdom, parentEl){
+    const { tag, props, children } = vdom
+
+    const element = document.createElement(tag)
+    addProps(element, props, vdom)
+    vdom.el = element
+
+    children.forEach((child) => mountDOM(child, element))
+    parentEl.append(element)
+}
+
+function addProps(el, props, vdom){
+    const { on: events, ...attrs } = props
+    vdom.listeners = addEventListeners(events, el)
+
+    setAttributes(el, attrs)
+}
