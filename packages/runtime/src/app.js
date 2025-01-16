bac712668fc27b68960396f56676181d9029674e
Ravi Shrestha <rshrestha42@gatech.edu>
2025-01-15 20:12:39 -0600

    Add the initial dispatcher and renderen

---
+++ packages/runtime/src/app.js
@@ -0,0 +1,38 @@
+import { destroyDOM } from "./destroy-dom"
+
+import { Dispatcher } from "./dispatcher"
+import { mountDOM } from "./mount-dom"
+
+export function createApp({ state, view, reducers={} }) {
+    let parentEl = null
+    let vdom = null
+
+    const dispatcher = new Dispatcher()
+    const subscriptions = [dispatcher.afterEveryCommand(renderApp)]
+
+    for (const actionName in reducers) {
+        const reducer = reducers[actionName]
+
+        const subs = dispatcher.subscribe(actionName, (payload) => {
+            state = reducer(state, payload)
+        })
+
+        subscriptions.push(subs)
+    }
+
+    function renderApp(){
+        if (vdom) {
+            destroyDOM(vdom)
+        }
+
+        vdom = view(state)
+        mountDOM(vdom, parentEl)
+    }
+
+    return {
+        mount(_parentEl) {
+            parentEl = _parentEl
+            renderApp()
+        },
+    }
+}
