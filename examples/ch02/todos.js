670d7209347d09cee19441d295b4d7a7003506ae
Ravi Shrestha <shrestharavee1@gmail.com>
2025-01-22 21:26:29 -0600

    Start refactoring the todo basic app to use the our framework

--- examples/ch02/todos.js
+++ examples/ch02/todos.js
@@ -1,110 +1,59 @@
-// State of the app
-const todos = ['Walk the dog', 'Water the plants', 'Sand the chairs']
+import { createApp, h, hFragment } from 'https://unpkg.com/shrestr3-fe-fwk'
 
-// HTML element references
-const addTodoInput = document.getElementById('todo-input')
-const addTodoButton = document.getElementById('add-todo-btn')
-const todosList = document.getElementById('todos-list')
-
-// Initialize the view
-for (const todo of todos){
-    todosList.append(renderTodoInReadMode(todo))
+const state = {
+    currentTodo: '',
+    edit: {
+        idx: null,
+        original: null, 
+        edited: null,
+    },
+    todos: ['Walk the dog', 'Water the plants'],
 }
 
-addTodoInput.addEventListener('input', ()=>{
-    addTodoButton.disabled = addTodoInput.value.length < 3
-})
+const reducers = {
+    'update-current-todo': (state, currentTodo) => ({
+        ...state,
+        currentTodo,
+    }),
 
-addTodoInput.addEventListener('keydown', ({key})=> {
-    if (key === 'Enter' && addTodoInput.value.length > 3){
-        addTodo()
-    }
-})
+    'add-todo': (state) => ({
+        ...state,
+        currentTodo: '',
+        todos: [...state.todos, state.currentTodo]
+    }),
 
-addTodoButton.addEventListener('click', ()=> {
-    addTodo()
-})
+    'start-editing-todo': (state, idx) => ({
+        ...state,
+        edit: {
+            idx,
+            original: state.todos[idx],
+            edited: state.todos[idx]
+        }
+    }),
 
-// Functions
-function renderTodoInReadMode(todo){
-   
-    const li = document.createElement('li')
+    'edit-todo': (state, edited) => ({
+        ...state,
+        edit: { ...state, edited}
+    }),
+      
+    'save-edited-todo': (state) => {
+        const todos = [...state.todos]
+        todos[state.edit.idx] = state.edit.edited
 
-    const span = document.createElement('span')
+        return {
+            ...state,
+            edit: {idx: null, original: null, edited: null},
+            todos
+        }
+    },
 
-    span.textContent = todo
-    span.addEventListener('dblclick', ()=>{
-        const idx = todos.indexOf(todo)
-
-        todosList.replaceChild(
-            renderTodoInEditMode(todo),
-            todosList.childNodes[idx]
-        )
-    })
-    li.append(span)
-
-    const button = document.createElement('button')
-    button.textContent = "Done"
-    button.addEventListener('click', ()=> {
-        const idx = todos.indexOf(todo)
-        removeTodo(idx)
-    })
-    li.append(button)
-    return li
-}
-
-
-function renderTodoInEditMode(todo){
-    const li = document.createElement('li')
+    'cancel-editing-todo': (state) => ({ 
+        ...state,
+        edit: { idx: null, original: null, edited: null },     #13
+      }),
     
-    const input = document.createElement('input')
-    input.type = 'text'
-    input.value = todo
-    li.append(input)
-
-    const saveBtn = document.createElement('button')
-    saveBtn.textContent = 'Save'
-    saveBtn.addEventListener('click', ()=> {
-        const idx = todos.indexOf(todo)
-        updateTodo(idx, input.value)
-    })
-    li.append(saveBtn)
-
-
-    const cancelBtn = document.createElement('button')
-    cancelBtn.textContent = 'Cancel'
-    cancelBtn.addEventListener('click', ()=> {
-        const idx = todos.indexOf(todo)
-        todosList.replaceChild(
-            renderTodoInReadMode(todo),
-            todosList.childNodes[idx]
-        )
-    })
-    li.append(cancelBtn)
-    return li
-}
-
-function addTodo(){
-// TODO: Impplement me!   
-
-    const description = addTodoInput.value
-
-    todos.push(description)
-    const todo = renderTodoInReadMode(description)
-    todosList.append(todo)
-
-    addTodoInput.value = ''
-    addTodoButton.disabled = true
-}
-
-function removeTodo(index){
-    // TODO: Implement me!
-    todos.splice(index, 1)
-    todosList.childNodes[index].remove()
-}
-
-function updateTodo(index, description){
-    todos[index] = description
-    const todo = renderTodoInReadMode(description)
-    todosList.replaceChild(todo, todosList.childNodes[index])
+      'remove-todo': (state, idx) => ({           
+        ...state,
+        todos: state.todos.filter((_, i) => i !== idx),        #15
+      }),
 }
